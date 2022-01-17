1d48c9380be8715eedf740c18427fbac9bfb740f
Gabrielle <gabrielle.bhagwat@gmail.com>
2022-01-17 15:07:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+
+//every React component has access to this object called 'props'
+// 'props' object is going to hold all our properties for the component
+function Header(props){
+  console.log(props)
+  return (
+    <header>
+      <h1>Elle's Kitchen</h1>
+    </header>
+  )
+}
+
+function Main(){
+  return (
+    <section>
+      <p>We serve the most delicious food around.</p>
+    </section>
+  )
+}
+
+function Footer(){
+  return (
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <Main />
+        <Footer />
     </div>
   );
 }
